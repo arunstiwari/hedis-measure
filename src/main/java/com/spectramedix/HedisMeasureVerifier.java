592ae02609ecf498124f90c0fe14b27c3d97b826
arunstiwari <arunstiwari@gmail.com>
2021-06-10 09:49:09 +0530

    Added hedis jar utility

--- src/main/java/com/spectramedix/HedisMeasureVerifier.java
+++ src/main/java/com/spectramedix/HedisMeasureVerifier.java
@@ -42,7 +42,7 @@ public class HedisMeasureVerifier {
         List<List<String>> recordsList = records.subList(2, records.size() - 1);
         System.out.println("recordsList.size() = " + recordsList.size());
         recordsList.stream()
-                .filter(rec -> rec.size() > 1)
+//                .filter(rec -> rec.size() > 1)
                 .forEach(rec -> {
                     System.out.println("rec = " + rec.get(0)+"WBC");
                     System.out.println("rec = " + rec.get(0).trim()+"WBC");
